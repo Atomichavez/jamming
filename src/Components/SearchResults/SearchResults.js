4ce08dbdfdf4e52428312f85137b08042e894cb7
atomichavez <dchavez0628@gmail.com>
2022-03-31 00:45:17 -0600

    Working version

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,18 @@
+import './SearchResults.css'
+import React from 'react'
+import { Tracklist } from '../Tracklist/Tracklist'
+
+export class SearchResults extends React.Component {
+  render() {
+    return(
+      <div className="SearchResults">
+        <h2>Results</h2>
+        {/*Add a TrackList component*/}
+        <Tracklist
+          tracks={this.props.searchResults}
+          onAdd={this.props.onAdd}
+          isRemoval={false}/>
+      </div>
+    )
+  }
+}
